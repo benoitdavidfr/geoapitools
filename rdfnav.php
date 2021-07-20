da6cf679243e426d48cb69fe434c9a97f5961670
Benoit DAVID <benoit.davi@free.fr>
2021-07-20 14:59:39 +0000

    ajout rdfnav.php

--- rdfnav.php
+++ rdfnav.php
@@ -3,38 +3,57 @@
 name: rdfnav.php
 title: rdfnav.php - navigateur RDF
 doc: |
-  Le format RDF/XML est peu lisible. Turtle l'est beaucoup plus.
-  Turtle a cependant l'inconvénient
-    - d'une part de ne pas permettre de naviguer au sein des ressources cliquer sur les URI des ressources,
-    - d'autre part de ne pas s'afficher simplement dans le nigateur mais de provoquer un téléchargement.
-  Enfin, JSON-LD est plus lisible que RDF/XML mais moins que Turtle ; il est toutefois bien adapté pour générer du RDF.
 
-  L'objectif du script est de faciliter la navigation au sein de données liées exposées en RDF en:
-    - convertissant ces données en Turtle à partir de JSON-LD ou de RDF/XML,
-    - affichant ce Turtle en HTML avec de liens cliquables vers les ressources liées.
-  
-  Le script prend en paramètre un URL/URI de ressource.
-  Il effectue un GET sur cet URL/URI en positionnant l'en-tête Accept à
-    application/x-turtle, application/ld+json, application/rdf+xml
-  
-  Si le Content-Type est application/x-turtle ou text/plain ou text/turtle
-  alors pas de conversion
-  sinonSi le Content-Type est application/ld+json ou application/rdf+xml
-  alors conversion en Turtle
-  sinon
-    erreur
-  Affichage du Turtle formatté en Html
-   - avec une barre fournissant l'URI/URL,
-   - et permettant de clicker sur les liens en renvoyant vers le navigateur.
-   
-  Le seul paramètre du script est l'URL/URI en paramètre GET url.
+
 journal: |
   20/7/2021:
     - création
 */
 require __DIR__.'/vendor/autoload.php';
 
-// transforme le contenu de $http_response_header en un array
+$doc = <<<EOT
+<h2>Documentation</h2>
+Le format RDF/XML est peu lisible. Turtle l'est beaucoup plus.<br>
+Turtle a cependant comme inconvénients :<ul>
+  <li>de ne pas s'afficher simplement dans le navigateur mais de provoquer un téléchargement,
+  <li>de ne pas permettre de naviguer au sein des ressources en cliquant sur leur URI.
+</ul>
+Enfin, JSON-LD est plus lisible que RDF/XML mais moins que Turtle ; il est toutefois bien adapté pour générer du RDF.
+</p>
+
+L'objectif de ce script est de faciliter la navigation au sein de données liées exposées en RDF en:<ul>
+  <li>récupérant par un GET HTTP le contenu associé à un URI en demandant les formats Turtle, RDF/XML ou JSON-LD,
+  <li>convertissant ce contenu en Turtle lorsqu'il est en JSON-LD ou en RDF/XML,
+  <li>affichant ce Turtle en HTML avec de liens cliquables vers les ressources liées.
+</ul>
+
+Le script prend en paramètre GET url un URL/URI de ressource.<br>
+Il effectue un GET sur cet URL/URI en positionnant l'en-tête Accept à<br>
+  <code>application/x-turtle, application/ld+json, application/rdf+xml</code>
+</p>
+
+<h4>Pseudo-code</h4>
+<code>
+Si le Content-Type est application/x-turtle ou text/plain ou text/turtle<br>
+alors pas de conversion<br>
+sinonSi le Content-Type est application/ld+json ou application/rdf+xml<br>
+alors conversion en Turtle<br>
+sinon<br>
+&nbsp;&nbsp;erreur<br>
+
+Affichage du Turtle formatté en Html<br>
+ - avec une barre fournissant l'URI/URL et permettant d'en saisir un,<br>
+ - et permettant de cliquer sur les liens en renvoyant vers le navigateur.<br>
+</code>
+
+<h4>Exemples</h4>
+<ul>
+<li><a href='?url=http://localhost/geoapi/dido/api.php/v1/dcatexport.rdf'>http://localhost/geoapi/dido/api.php/v1/dcatexport.rdf</a>
+<li><a href='?url=https://dido.geoapi.fr/v1/dcatexport.jsonld'>https://dido.geoapi.fr/v1/dcatexport.jsonld</a>
+</ul>
+EOT;
+ 
+// transforme le contenu de $http_response_header en un array avec cls significatives
 function response_header(array $input): array {
   if (!$input) return ['error'=> '$http_response_header non défini'];
   $output = ['returnCode'=> array_shift($input)];
@@ -50,18 +69,19 @@ echo "<!DOCTYPE HTML><html><head><meta charset='UTF-8'><title>rdfnav</title></he
 //echo '<pre>'; print_r($_SERVER);
 $url = $_GET['url'] ?? '';
 
-echo "<table><tr>" // le formulaire d'affichage et de saisie de l'URL
+$form = "<tr>" // le formulaire d'affichage et de saisie de l'URL sous la forme d'une ligne d'une table HTML
       . "<td>url:</td>"
-      . "<td><table border=1><form><tr>"
-      . "<td><input type='text' size=130 name='url' value='$url'/></td>\n"
-      . "<td><input type='submit' value='Go'></td>\n"
-      . "</tr></form></table></td>\n"
-      . "</tr></table>\n";
+      . "<td><form>"
+      . "<input type='text' size=130 name='url' value='$url'/>\n"
+      . "<input type='submit' value='Get'>"
+      . "</form></td>\n"
+      . "</tr>\n";
+//echo "<table border=1>$form</table>\n";
 
 if (!$url)
-  die("Url vide");
+  die("<table border=1>$form<tr><td>statut</td><td><b>Url vide</b></td></tr></table>$doc");
 
-$opts = [
+$context = stream_context_create([
   'http'=> [
     'method'=> 'GET',
     'ignore_errors' => '1',
@@ -69,48 +89,45 @@ $opts = [
               ."Accept-language: en\r\n"
               ."Cookie: foo=bar\r\n",
   ],
-];
+]);
 //echo "url=$url\n";
-if (FALSE === $contents = @file_get_contents($url, false, stream_context_create($opts))) {
-  echo "<pre><b>ERREUR</b>, http_response_header = "; print_r(response_header($http_response_header)); echo "</pre>\n";
-  die();
-}
-
+$contents = @file_get_contents($url, false, $context);
 $response_header = response_header($http_response_header);
-if (!preg_match('!^HTTP/1\.. 200!', $response_header['returnCode'])) {
-  echo "<pre><b>ERREUR</b>, http_response_header = "; print_r($response_header); echo "</pre>\n";
+if (($contents === FALSE) || !preg_match('!^HTTP/1\.. 200!', $response_header['returnCode'])) {
+  echo "<table border=1>$form<tr><td>statut</td><td><pre><b>ERREUR</b>: ",
+       json_encode($response_header, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE),
+       "</pre></td></tr></table>\n";
   die();
 }
-
 //echo "<pre><b>OK</b>, http_response_header = "; print_r($response_header); echo "</pre>\n";
 
 if (preg_match('!^application/ld\+json!', $response_header['Content-Type'])) {
   $data = new \EasyRdf\Graph($url);
   $data->parse($contents, 'jsonld', $url);
-  $turtle = $data->serialise('turtle');
+  $contents = $data->serialise('turtle');
 }
 elseif (preg_match('!^(application/rdf\+xml|text/xml)!', $response_header['Content-Type'])) {
   $data = new \EasyRdf\Graph($url);
   $data->parse($contents, 'rdf', $url);
-  $turtle = $data->serialise('turtle');
+  $contents = $data->serialise('turtle');
 }
-elseif (preg_match('!^(application/x-turtle|text/plain|text/turtle)!', $response_header['Content-Type'])) {
-  $turtle = $contents;
-}
-else {
-  die("En-tête ".$response_header['Content-Type']." non comprise\n");
+elseif (!preg_match('!^(application/x-turtle|text/plain|text/turtle)!', $response_header['Content-Type'])) {
+  die("<table border=1>$form"
+      ."<tr><td>statut</td><td><b>En-tête ".$response_header['Content-Type']." non comprise</b></td></tr>"
+      ."</table>\n");
 }
 
-echo '<pre>';
+echo "<table border=1>$form",
+     "<tr><td>Content-Type</td><td>",$response_header['Content-Type'],"</td></tr>",
+     "</table>\n",
+     '<pre>';
 
-// le texte à afficher est dans $turtle
-while (preg_match('!<(http[^>]+)>!', $turtle, $matches)) {
+// le texte à afficher est en Turtle dans $contents
+while (preg_match('!<(http[^>]+)>!', $contents, $matches)) {
   //print_r($matches);
   $url = $matches[1];
   $replacement = "<a href='?url=".urlencode($url)."'>$url</a>";
-  $turtle = preg_replace('!<http[^>]+>!', "&lt;$replacement&gt;", $turtle, 1);
-  //die();
-  //break;
+  $contents = preg_replace('!<http[^>]+>!', "&lt;$replacement&gt;", $contents, 1);
 }
 
-echo $turtle;
+echo $contents;
